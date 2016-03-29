bc56f06392b99ca12b4bacd448fc1ac10db10ff9
MaggieGao <MaggieGao@users.noreply.github.com>
2016-03-29 12:00:49 -0400

    Update PiPair_eclipse.java

--- PiPair_eclipse.java
+++ PiPair_eclipse.java
@@ -1,41 +1,44 @@
 package pipair;
 
 import java.util.Scanner;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.io.File;
 import java.io.IOException;
 import java.text.NumberFormat;
-import java.util.HashMap;
+import java.util.Map;
 
 public class PiPair {
 	
-	/* record function(s) called by it(their) caller(s) as value in HashMap pipair, 
+	/* store function(s) called by it(their) caller(s) as value in HashMap pipair, 
 	 * use HashSet to eliminate duplicate functions */
-	static HashSet<String> stringset = new HashSet<String>();
+	public static Set<String> stringset = new HashSet<String>();
 	
-	/* record function(s) called by it(their) caller(s) as one value-key set */
-	static HashMap<HashSet<String>, HashSet<String>> pipair = new HashMap<HashSet<String>, HashSet<String>>();
+	/* record function(s) and it(their) caller(s) as key-value set in Map*/
+	public static Map<HashSet<String>, HashSet<String>> pipair = new HashMap<HashSet<String>, HashSet<String>>();
 	
 	public static void main(String [] args) throws Exception{
 		
 		int support = 3;
-		float confidence = 65;
+		float confidence = (float)0.65;
 		
-		int args_length = args.length;
-		if(args_length >= 1){
-			support = Integer.parseInt(args[0]);
-			if(args_length == 2){
+		switch(args.length){
+			case 0: break;
+			case 2: 
+				support = Integer.parseInt(args[0]);
 				confidence = Float.parseFloat(args[1])/100;
-			} else if(args_length > 2){
-				System.err.println("Error: Wrong parameter input.");
-			}
+				break;
+			default:
+				System.err.println("Error: Wrong arguments input.");
+                System.exit(1);
 		}
 		
-		System.out.println("support = " + support);
-		System.out.println("confidence = " + confidence);
-		
+		/* read callgraph file into profram and store in map */
 		readFile();
 		findBug(support, confidence);
 		
@@ -47,6 +50,7 @@ public class PiPair {
 		//Scanner scanner = new Scanner(System.in);
 		
 		// test 1 input data:
+		/*
 		String note = "Call graph node <<null function>><<0x12f5f210>>  #uses=0\n"
 				+ "CS<0x0> calls function 'main'\n"
 				+ "CS<0x0> calls function 'printf'\n"
@@ -62,11 +66,12 @@ public class PiPair {
 				+ "Call graph node for function: 'A'<<0x12f62eb0>>  #uses=2\n"
 				+ "CS<0x12f67d70> calls function 'printf'\n"
 				+ "CS<0x12f67e10> calls function 'printf'\n";
-		
+		*/
 		//Scanner scanner = new Scanner(note);
 		
 		// test 2:
-		File file = new File("C:" + File.separator + "input.txt");
+		File file = new File("C:" + File.separator + "graph.txt");
+		//File file = new File(File.separator + "input.txt");
 		Scanner scanner = new Scanner(file);
 		
 		String pattern_caller = "Call.*'(.*)'.*";
@@ -114,9 +119,9 @@ public class PiPair {
 		
 		HashSet<String> map_value = null;
 		HashSet<String> map_key = new HashSet<String>();
- 		map_key.add(add_key);
 		
  		/* map_key only has one element */
+ 		map_key.add(add_key);
  		if(pipair.containsKey(map_key)){
 			map_value = pipair.get(map_key);
 		} else{
@@ -147,16 +152,14 @@ public class PiPair {
 	
 	static void findBug(int support, float confidence){
 		
-		/* value is the number of elements in pipair's value, 
-		 * two HashMap separate pipairs whose keys include one or two elements*/
-		
+		/*  two HashMap separate pipairs whose keys include one element and two, 
+		 *  key is the same as that in pipair,
+		 *  value is the number of elements of the corresponding value in pipair */
 		HashMap<HashSet<String>, Integer> pipair_single = new HashMap<HashSet<String>, Integer>();
 		HashMap<HashSet<String>, Integer> pipair_pair = new HashMap<HashSet<String>, Integer>();
 		
-		//Iterator<E> entries = pipair.entrySet().iterator();
-		//while(entries.hasNext())
-			
-		for(HashMap.Entry<HashSet<String>, HashSet<String>> entry : pipair.entrySet()){
+		/* construct pipair_single and pipair_pair from pipair */
+		for(Map.Entry<HashSet<String>, HashSet<String>> entry : pipair.entrySet()){
 			HashSet<String> key = entry.getKey();
 			HashSet<String> value = entry.getValue();
 			int key_count = key.size();
@@ -173,7 +176,12 @@ public class PiPair {
 		//System.out.println("pipair_single: " + pipair_single.toString());
 		//System.out.println("pipair_pair: " + pipair_pair.toString());
 		
-		for(HashMap.Entry<HashSet<String>, Integer> entry_single : pipair_single.entrySet()){
+		/*  key_pair<HashSet<String>> -> value_pair<Integer>,
+		 *  key_single<String> -> value_single<Integer>  
+		 * 
+		 *  key_pair<HashSet<String>> -> pair<HashSet<String>>, 
+		 *  key_single<String> -> single<HashSet<String>> */
+		for(Map.Entry<HashSet<String>, Integer> entry_single : pipair_single.entrySet()){
 			int value_single = entry_single.getValue();
 			HashSet<String> key_s = entry_single.getKey();
 			String key_single = key_s.toString();
@@ -181,12 +189,12 @@ public class PiPair {
 			key_single = key_single.replaceAll("\\]", "");
 			
 			if(value_single >= support){
-				for(HashMap.Entry<HashSet<String>, Integer> entry_pair : pipair_pair.entrySet()){
+				for(Map.Entry<HashSet<String>, Integer> entry_pair : pipair_pair.entrySet()){
 					int value_pair = entry_pair.getValue();
 					HashSet<String> key_pair = entry_pair.getKey();
 					float confi = (float)value_pair / value_single;
 					
-					if(value_pair >= support && key_pair.contains(key_single) && confi > confidence && confi < 1){		
+					if(value_pair >= support && key_pair.contains(key_single) && confi >= confidence && confi <= 1){		
 						HashSet<String> single = null;
 						HashSet<String> pair = null;
 						single = pipair.get(key_s);  // not key_single, it is a String not HashSet
@@ -194,13 +202,25 @@ public class PiPair {
 						for(String s:single){
 							if(!pair.contains(s)){
 								/* printing bug */
-								String bug_pair = key_pair.toString();
+								
+								ArrayList<String> bug_pair_sort = new ArrayList<String>();
+								for(String str:key_pair){
+									bug_pair_sort.add(str);
+								}
+								Collections.sort(bug_pair_sort);
+								
+								String bug_pair = bug_pair_sort.toString();
 								bug_pair = bug_pair.replaceAll("\\[", "(");
 								bug_pair = bug_pair.replaceAll("\\]", ")");
+								
+								NumberFormat fmt = NumberFormat.getPercentInstance();
+								fmt.setMinimumFractionDigits(2);
+								fmt.setMaximumFractionDigits(2);
+								
 								System.out.println("bug: " + key_single + " in "+ s 
 									+ ", pair: " + bug_pair
-									+ ", support: " + Integer.toString(value_pair)
-									+ ", confidence: " + NumberFormat.getPercentInstance().format(confi)
+									+ ", support: " + value_pair
+									+ ", confidence: " + fmt.format(confi)
 									);
 							}	
 						}
